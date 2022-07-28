b9712c56b3042b29b0515b4ef75495db8aea4b72
Cabral dos Santos <52243232+cabraldossantos@users.noreply.github.com>
2022-07-28 13:11:05 -0300

    Create idade.py
    Inclusão do arquivo idade.py.

---
+++ python/idade.py
@@ -0,0 +1,22 @@
+class Idade:
+    def __init__(self, nome, ano_nascimento):
+        self.nome = ""
+        self.ano_nascimento = 0
+
+    def verificar_ano(self):
+        return True
+
+    def calcular_idade(self):
+        return 0
+
+
+
+
+
+idade = Idade()
+
+nome = input("Informe seu nome: ")
+ano_nascimento = int(input("Informe sua data de nascimento: "))
+
+
+
