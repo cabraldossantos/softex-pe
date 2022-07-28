b02e30fbc71c19ccfde5a41a369c2dcd355fc7ad
Cabral dos Santos <52243232+cabraldossantos@users.noreply.github.com>
2022-07-28 17:57:43 -0300

    Update idade.py
    Finalização da primeira versão do arquivo idade.py.

--- python/idade.py
+++ python/idade.py
@@ -1,13 +1,24 @@
+from datetime import datetime
+
+
 class Idade:
-    def __init__(self, nome, ano_nascimento):
+    def __init__(self):
         self.nome = ""
         self.ano_nascimento = 0
+        # self.verificar_ano()
 
     def verificar_ano(self):
-        return True
+        verificado = False
+        if ((self.ano_nascimento >= 1922) and (self.ano_nascimento <= 2021)):
+            verificado = True
+        return verificado
 
-    def calcular_idade(self):
-        return 0
+    def calcular(self):
+        ano_atual = datetime.now()
+        ano_atual = int(ano_atual.strftime("%Y"))
+        completou = int((ano_atual - 1) - self.ano_nascimento)
+        completara = int(ano_atual - self.ano_nascimento)
+        return "Prezado(a) " + self.nome + ", você completou " + str(completou) + " anos.\nE em " + str(ano_atual) + " completará " + str(completara) + " anos."
 
 
 
@@ -15,8 +26,16 @@ class Idade:
 
 idade = Idade()
 
-nome = input("Informe seu nome: ")
-ano_nascimento = int(input("Informe sua data de nascimento: "))
+while not idade.verificar_ano():
+    try:
+        idade.nome = input("Informe seu nome: ")
+        idade.ano_nascimento = int(input("Informe o seu ano de nascimento: "))
+    except Exception as err:
+        print("\nOcorreu um erro: " + str(err))
 
-
-
+    if idade.verificar_ano():
+        print(idade.calcular())
+    else:
+        print()
+        print("O ano de nascimento deve estar entre 1922 e 2021.\n")
+        
