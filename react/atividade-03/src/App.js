dc335dbac4e753d1dd663b08a607c1f9f31763b3
Cabral dos Santos <52243232+cabraldossantos@users.noreply.github.com>
2022-09-16 21:35:49 -0300

    Inclusão da atividade 03.

---
+++ react/atividade-03/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Treinando React.JS na SOFTEX - PE" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg" alt="React JS" width="200px" />
+    </div>
+  )
+}
+
+export default App
